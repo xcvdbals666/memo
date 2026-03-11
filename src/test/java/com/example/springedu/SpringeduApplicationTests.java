46672a0dee4b8a035b62c101ecf182602176d03e
xcvdbals666 <xcvdbals666@naver.com>
2026-03-11 15:38:33 +0900

    test

--- src/test/java/com/example/springedu/SpringeduApplicationTests.java
+++ src/test/java/com/example/springedu/SpringeduApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.springedu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpringeduApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
